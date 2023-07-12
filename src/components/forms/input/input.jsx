36b0ac414ee1ecd55b9cbe6fbdd6e583cba4b08a
Helton Barbosa <helton1000xd8@gmail.com>
2023-07-12 14:45:42 -0300

    Craido input e atualizado a navbar

---
+++ src/components/forms/input/input.jsx
@@ -0,0 +1,8 @@
+import style from './input.module.css'
+
+export default function Input({fullWidth, ...props}){
+    return(
+        <input className={`${style.input} ${fullWidth && style.fullwidth}`}  
+        {...props} />
+    )
+}
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,12 +1,15 @@
 import styles from './navbar.module.css'
 import { BsCart4 } from 'react-icons/bs'
+import Input from '@/components/forms/input/input'
 import Logo from '@/components/logo/logo'
 
 export default function Navbar(){
     return(
         <nav className={styles.navbar}>
             <Logo />
-            <input />
+            <div className={styles.search}>
+                <Input placeholder="Buscar" fullWidth/>
+            </div>
             <BsCart4 size={40} />
         </nav>
     )
