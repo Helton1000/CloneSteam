122abbc4b17a3446148103528831fadba94c836b
Helton Barbosa <helton1000xd8@gmail.com>
2023-07-12 13:48:07 -0300

    criado layout de promocoes no index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import styles from '@/styles/index.module.css'
 import Navbar from '@/components/navbar/navbar'
 import Subtitle from '@/components/tipography/subtitle/subtitle'
 import Container from '@/components/container/container'
@@ -16,11 +17,17 @@ export default function Home() {
       <div>
         <Navbar />
         <Container>
-          <div>
+          <div className={styles.session}>
             <Subtitle>Promoções</Subtitle>
+            <div className={styles.salecontainer}>
+              <SaleCard/>
+              <SaleCard/>
+              <SaleCard/>
+            </div>
           </div>
-          <SaleCard/>
-          <div>
+
+          
+          <div className={styles.session}>
             <Subtitle>Outros Jogos</Subtitle>
           </div>
         </Container>
