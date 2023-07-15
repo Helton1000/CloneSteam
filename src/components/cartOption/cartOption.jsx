ed581bbb34b782da3fd854edbe7185f0801d3405
Helton Barbosa <helton1000xd8@gmail.com>
2023-07-15 11:28:04 -0300

    Criado cartMenu

--- src/components/cartOption/cartOption.jsx
+++ src/components/cartOption/cartOption.jsx
@@ -3,12 +3,12 @@ import styles from './cartOption.module.css'
 
 export default function CartOption(){
     return(
-        <div>
+        <div className={styles.option}>
             <Image src={`/products/league-of-legends.jpg`} width={62} height={74}/>
-            <div>
-                <h3>League of Legends</h3>
-                <h3>R$ 99,90</h3>
-                <p>remover</p>
+            <div className={styles.info}>
+                <h3 className={styles.title}>League of Legends</h3>
+                <h3 className={styles.price}>R$ 99,90</h3>
+                <p className={styles.remove}>remover</p>
             </div>
         </div>
     )
